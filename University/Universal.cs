6364ff5b3d3ce2645b3bbd2c0380adc179638f51
Ali Najafi <39293965+AliNajafi1998@users.noreply.github.com>
2019-01-27 12:37:37 +0330

    Back to Universal

--- University/Universal.cs
+++ University/Universal.cs
@@ -1,66 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
-namespace University
+namespace UniversityClasses
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.IO;
-    using System.Runtime.Serialization.Formatters.Binary;
-    using UniversityClasses;
-
-    namespace University
+    [System.Serializable]
+    class Universal
     {
-        [System.Serializable]
-        class University
+        public static Universal instance;
+
+        /**
+         * Initializes main file and BTrees.
+         */
+        static Universal()
         {
-            public static University instance;
-            static University()
+            if (instance == null)
             {
-                if (instance == null)
+                instance = new Universal();
+                if (File.Exists("Uni"))
                 {
-                    instance = new University();
-                    if (File.Exists("Uni"))
+                    //File exists.
+                    FileStream file = File.Open("Uni", FileMode.Open);
+                    BinaryFormatter bf = new BinaryFormatter();
+                    if (new FileInfo("Uni").Length == 0)
                     {
-                        //File exists.
-                        FileStream file = File.Open("Uni", FileMode.Open);
-                        BinaryFormatter bf = new BinaryFormatter();
-                        if (new FileInfo("Uni").Length == 0)
-                        {
-                            //File is empty.
-                            BTree newManager = new BTree();
-                            Manager manager = new Manager();
-                            newManager.put(manager.id + "", "mng\\" + manager.id);
-                            instance.managerTree = newManager;
-                            bf.Serialize(file, instance);
-                            FileStream newFile = File.Create("mng\\" + manager.id);
-                            BinaryFormatter newbf = new BinaryFormatter();
-                            bf.Serialize(newFile, manager);
-                            newFile.Close();
-                            file.Close();
-                        }
-                        else
-                        {
-                            //File was already created.
-                            instance = bf.Deserialize(file) as University;
-                            file.Close();
-                        }
-                    }
-                    else
-                    {
-                        //File not exists
+                        //File is empty.
                         BTree newManager = new BTree();
                         Manager manager = new Manager();
                         newManager.put(manager.id + "", "mng\\" + manager.id);
                         instance.managerTree = newManager;
-                        FileStream file = File.Create("Uni");
-                        BinaryFormatter bf = new BinaryFormatter();
                         bf.Serialize(file, instance);
                         FileStream newFile = File.Create("mng\\" + manager.id);
                         BinaryFormatter newbf = new BinaryFormatter();
@@ -68,16 +35,38 @@ namespace University
                         newFile.Close();
                         file.Close();
                     }
+                    else
+                    {
+                        //File was already created.
+                        instance = bf.Deserialize(file) as Universal;
+                        file.Close();
+                    }
+                }
+                else
+                {
+                    //File not exists
+                    BTree newManager = new BTree();
+                    Manager manager = new Manager();
+                    newManager.put(manager.id + "", "mng\\" + manager.id);
+                    instance.managerTree = newManager;
+                    FileStream file = File.Create("Uni");
+                    BinaryFormatter bf = new BinaryFormatter();
+                    bf.Serialize(file, instance);
+                    FileStream newFile = File.Create("mng\\" + manager.id);
+                    BinaryFormatter newbf = new BinaryFormatter();
+                    bf.Serialize(newFile, manager);
+                    newFile.Close();
+                    file.Close();
                 }
             }
-            public BTree managerTree;       //Tree of managers
-            public BTree masterTree;        //Tree of masters
-            public BTree studentTree;       //Tree of students
-            public BTree courseTree;        //Tree of courses
-
-            public bool isAbleUnitChoice = false;       //UnitChoice ability
-            public bool isAbleUnitEdit = false;         //UnitEdit ability
         }
-    }
 
+        public BTree studentTree;       //Tree of students
+        public BTree masterTree;        //Tree of masters
+        public BTree managerTree;       //Tree of managers
+        public BTree courseTree;        //Tree of courses
+
+        public bool isAbleUnitChoice = false;       //UnitChoice ability
+        public bool isAbleUnitEdit = false;         //UnitEdit ability
+    }
 }
